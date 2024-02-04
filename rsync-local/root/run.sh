515c5c46c20cd4fa2c0258b59d0ff1e75b1d54e5
Hidri02 <68425351+Hidri02@users.noreply.github.com>
2024-02-04 01:24:45 +0100

    Update run.sh
    add local variable

--- rsync-local/root/run.sh
+++ rsync-local/root/run.sh
@@ -17,24 +17,39 @@ else
   bashio::log.info "Starting sync..."
   EXTERNAL_DEVICE=$(bashio::config 'external_device')
 
-
-  bashio::log.info "Mounting device ${EXTERNAL_DEVICE}"
-  mkdir -p /external
-  mount "${EXTERNAL_DEVICE}" /external
-
-  folder_count=$(echo "$FOLDERS" | jq -r '. | length')
-  for (( i=0; i<folder_count; i=i+1 )); do
-
-    local=$(echo "$FOLDERS" | jq -r ".[$i].source")
-    options=$(echo "$FOLDERS" | jq -r ".[$i].options // \"--archive --recursive --compress --delete --prune-empty-dirs\"")
-    bashio::log.info "Sync ${local} -> ${EXTERNAL_DEVICE}/${EXTERNAL_FOLDER} with options \"${options}\""
-    set -x
-    # shellcheck disable=SC2086
-    rsync ${options} \
-    "$local" "/external/${EXTERNAL_FOLDER}"
-    set +x
-  done
-
-  umount /external
-  bashio::log.info "Synced all folders"
+  if [[ "$EXTERNAL_DEVICE" == "local" ]]; then  
+  
+    folder_count=$(echo "$FOLDERS" | jq -r '. | length')
+    for (( i=0; i<folder_count; i=i+1 )); do
+      source=$(echo "$FOLDERS" | jq -r ".[$i].source")
+      options=$(echo "$FOLDERS" | jq -r ".[$i].options // \"--archive --recursive --compress --delete --prune-empty-dirs\"")
+      bashio::log.info "Sync ${local} -> ${EXTERNAL_DEVICE}/${EXTERNAL_FOLDER} with options \"${options}\""
+      set -x
+      # shellcheck disable=SC2086
+      rsync ${options} \
+      "$source" "${EXTERNAL_FOLDER}"
+      set +x
+    done
+  
+    bashio::log.info "Synced all folders"
+  else
+    bashio::log.info "Mounting device ${EXTERNAL_DEVICE}"
+    mkdir -p /external
+    mount "${EXTERNAL_DEVICE}" /external
+  
+    folder_count=$(echo "$FOLDERS" | jq -r '. | length')
+    for (( i=0; i<folder_count; i=i+1 )); do
+  
+      local=$(echo "$FOLDERS" | jq -r ".[$i].source")
+      options=$(echo "$FOLDERS" | jq -r ".[$i].options // \"--archive --recursive --compress --delete --prune-empty-dirs\"")
+      bashio::log.info "Sync ${local} -> ${EXTERNAL_DEVICE}/${EXTERNAL_FOLDER} with options \"${options}\""
+      set -x
+      # shellcheck disable=SC2086
+      rsync ${options} \
+      "$local" "/external/${EXTERNAL_FOLDER}"
+      set +x
+    done
+  
+    umount /external
+    bashio::log.info "Synced all folders"
 fi
